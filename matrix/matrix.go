04cdefef7ea29659103da3e7576134c130abb854
George Lonshakov <miltendean@gmail.com>
2020-05-04 18:35:18 +0300

    matrix commit

---
+++ matrix/matrix.go
@@ -0,0 +1,43 @@
+package matrix
+
+//go:generate genny -in=$GOFILE -out=int/dont_edit.go gen "ValueType=int"
+
+import "github.com/cheekybits/genny/generic"
+
+type ValueType generic.Type
+
+type Matrix struct {
+	Rows int // number of rows
+	Cols int // number of columns
+	// contains filtered or unexported fields
+	matr [][]ValueType
+}
+
+func New(n, m int) *Matrix {
+	var matrrr [][]ValueType
+	mtrx := Matrix{
+		n,
+		m,
+		matrrr,
+	}
+	for i := 0; i < mtrx.Rows; i++ {
+		row := make([]ValueType, mtrx.Cols)
+		mtrx.matr = append(mtrx.matr, row)
+	}
+	smth := &mtrx
+	return smth
+}
+
+func (M *Matrix) Set(i, j int, v ValueType) {
+	if i >= M.Rows || i < 0 || j >= M.Cols || j < 0 {
+		panic("indice is out of range")
+	}
+	M.matr[i][j] = v
+}
+
+func (M *Matrix) Get(i, j int) ValueType {
+	if i >= M.Rows || i < 0 || j >= M.Cols || j < 0 {
+		panic("indice is out of range")
+	}
+	return M.matr[i][j]
+}
