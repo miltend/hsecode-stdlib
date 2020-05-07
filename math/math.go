3360b044382a262a39e4e776ae935833d1886c00
George Lonshakov <miltendean@gmail.com>
2020-05-07 16:45:57 +0300

    nth prime commit

--- math/math.go
+++ math/math.go
@@ -33,7 +33,7 @@ func NthPrime(n int) int {
 		}
 		return primes[n-1]
 	default:
-		const maxSize = 181000000
+		const maxSize = 180495000
 		isPrime := make([]bool, maxSize)
 		var primes []int
 		for i := 2; i < maxSize; i++ {
