33b75ee3eef391e14dac44cd5a7a72bebe4bd249
George Lonshakov <miltendean@gmail.com>
2020-05-07 16:08:46 +0300

    nth prime commit

--- math/math_test.go
+++ math/math_test.go
@@ -10,13 +10,13 @@ func TestCompare(t *testing.T) {
 	if math.NthPrime(3) != 5 {
 		t.Fatal("unexpected prime value")
 	}
-	if math.NthPrime(24368) != 279121 {
+	if math.NthPrime(141) != 811 {
 		t.Fatal("unexpected prime value")
 	}
 	if math.NthPrime(3) != 5 {
 		t.Fatal("unexpected prime value")
 	}
-	if math.NthPrime(24368) != 279121 {
+	if math.NthPrime(141) != 811 {
 		t.Fatal("unexpected prime value")
 	}
 	if math.NthPrime(3) != 5 {
