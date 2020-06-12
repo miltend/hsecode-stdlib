e61d84bd001318f99b848b03e1c4ebc37d725877
George Lonshakov <miltendean@gmail.com>
2020-06-12 17:28:56 +0300

    sym

---
+++ tree/sym.go
@@ -0,0 +1,39 @@
+package tree
+
+func (T *Tree) IsSym() bool {
+	if T == nil {
+		return true
+	}
+	if T.Left == nil && T.Right == nil {
+		return true
+	}
+	if T.Left == nil || T.Right == nil {
+		return false
+	}
+	return isAntySym(T.Left, T.Right)
+}
+
+func isAntySym(a, b *Tree) bool {
+	var isAS bool
+	if a.Value != b.Value {
+		return isAS
+	}
+
+	if a.Right != nil && b.Left != nil {
+		isAS = isAntySym(a.Right, b.Left)
+		if !isAS {
+			return isAS
+		}
+	}
+
+	if a.Left != nil && b.Right != nil {
+		isAS = isAntySym(a.Left, b.Right)
+		if !isAS {
+			return isAS
+		}
+	}
+
+	isAS = true
+	return isAS
+
+}
