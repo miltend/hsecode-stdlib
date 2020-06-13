7105d4e4308a37b2dc38475baa1dbe991fd898ee
George Lonshakov <miltendean@gmail.com>
2020-06-13 19:22:50 +0300

    lru

--- lru/lru.go
+++ lru/lru.go
@@ -4,6 +4,7 @@ package lru
 
 import (
 	"container/list"
+	//"fmt"
 )
 
 type Cache struct {
@@ -39,18 +40,22 @@ func (cache *Cache) Get(key int) (int, bool) {
 		return 0, false
 	}
 	cache.elements.MoveToFront(elem)
+	//if cache.elements.Len() == cache.capacity+1 {
+	//	cache.purge()
+	//}
 	return elem.Value.(*pair).value, true
 
 }
 
 func (cache *Cache) Put(key int, value int) {
 
-	if elem, ok := cache.ht[key]; ok == true {
-		cache.elements.MoveToFront(elem)
-		elem.Value.(*pair).value = value
-	}
+	//
+	//if elem, ok := cache.ht[key]; ok == true {
+	//	cache.elements.MoveToFront(elem)
+	//	elem.Value.(*pair).value = value
+	//}
 
-	if cache.elements.Len() == cache.capacity+1 {
+	if cache.elements.Len() == cache.capacity {
 		cache.purge()
 	}
 
@@ -61,20 +66,39 @@ func (cache *Cache) Put(key int, value int) {
 
 	elem := cache.elements.PushFront(item)
 	cache.ht[item.key] = elem
+
 }
 
-//
-//
 //func main() {
-//	a:= New(3)
-//	a.Put(1, 1)
-//	a.Put(2, 2)
-//	a.Put(3, 3)
-//	a.Put(1, 10)
-//	a.Put(4, 4)
-//	fmt.Println(a.Get(1))
-//	fmt.Println(a.Get(2))
-//	fmt.Println(a.Get(3))
-//	fmt.Println(a.Get(4))
+//a:= New(3)
+//a.Put(1, 1)
+//a.Put(2, 2)
+//a.Put(3, 3)
+//a.Put(1, 10)
+////for e := a.elements.Front(); e != nil; e = e.Next(){
+////	fmt.Println(e.Value)
+////}
+//a.Put(4, 4)
+////for e := a.elements.Front(); e != nil; e = e.Next(){
+////	fmt.Println(e.Value)
+////}
+//fmt.Println(a.Get(1))
 //
+////for e := a.elements.Front(); e != nil; e = e.Next(){
+////	fmt.Println(e.Value)
+////}
+//fmt.Println(a.Get(2))
+//fmt.Println(a.Get(3))
+//fmt.Println(a.Get(4))
+
+//for e := a.elements.Front(); e != nil; e = e.Next(){
+//	fmt.Println(e.Value)
+//}
+//fmt.Println(a.ht)
+//a:= New(2)
+//a.Put(1, 1)
+//a.Put(2, 2)
+//a.Put(3, 3)
+//fmt.Println(a.Get(1))
+
 //}
